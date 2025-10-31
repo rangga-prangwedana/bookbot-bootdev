65229e47dbd4d09016d880ecba899234afa07aee
Rangga Prangwedana <vergil1st@gmail.com>
2025-10-31 09:25:14 +0700

    fix: Added count character function to stats.py

--- stats.py
+++ stats.py
@@ -1,7 +1,17 @@
 def count_book_words(file_contents: str):
     """Count the total words of a text book."""
-    total_words = 0
+    return len(file_contents.split())
+
+
+def count_book_chars(file_contents: str):
+    """Return a dictionary with total count of each characters in a book."""
+    total_characters = {}
     for word in file_contents.split():
-        if word != "":
-            total_words += 1
-    return total_words
+        for char in word:
+            if not char == " ":
+                if char.lower() not in total_characters:
+                    total_characters[char.lower()] = 1
+                else:
+                    total_characters[char.lower()] += 1
+    return total_characters
+    
