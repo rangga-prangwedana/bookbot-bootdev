ceae2ce24053b82b25b73543e0c6c76b439b8b69
Rangga Prangwedana <vergil1st@gmail.com>
2025-10-31 13:51:01 +0700

    fix: Sorted char count function and limits to alphabet

--- stats.py
+++ stats.py
@@ -6,12 +6,15 @@ def count_book_words(file_contents: str):
 def count_book_chars(file_contents: str):
     """Return a dictionary with total count of each characters in a book."""
     total_characters = {}
-    for word in file_contents.split():
-        for char in word:
-            if not char == " ":
-                if char.lower() not in total_characters:
-                    total_characters[char.lower()] = 1
-                else:
-                    total_characters[char.lower()] += 1
-    return total_characters
+    # Not using split() means file_contents is a very long string.
+    for char in file_contents: 
+        if char.isalpha():
+            lower = char.lower()
+            if lower in total_characters:
+                total_characters[lower] += 1
+            else:
+                total_characters[lower] = 1
+    sorted_total_char = {key:value for key,value in sorted(total_characters.items(), key=lambda item: item[1], reverse=True)}
+
+    return sorted_total_char
     
