78062da39b5be6cd2024a0471b073bb6ce8393df
Rangga Prangwedana <vergil1st@gmail.com>
2025-11-01 13:38:14 +0700

    feat: Added sys.argv for calling code from CLI

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
+import sys
 from stats import count_book_words, count_book_chars
 
 
-def get_book_text(filepath: str = "books/frankenstein.txt"):
+def get_book_text(filepath: str):
     """Read the content of a text book."""
-    with open(filepath, encoding="utf-8") as f:
-        file_contents = f.read()
-    return file_contents
+    try:
+        with open(filepath, encoding="utf-8") as f:
+            file_contents = f.read()
+        return file_contents
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: {filepath} was not found.")
+    except Exception as e:
+        raise RuntimeError(f"An unexpected error occurred while reading '{filepath}': {e}") from e
+
         
 
 def main():
-    book_path = "books/frankenstein.txt"
-    book_contents = get_book_text(book_path)
-    total_words = count_book_words(book_contents)
-    total_chars = count_book_chars(book_contents)
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <path_to_file>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    try:
+        book_contents = get_book_text(book_path)
+        total_words = count_book_words(book_contents)
+        total_chars = count_book_chars(book_contents)
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {total_words} total words.")
-    print("--------- Character Count -------")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {total_words} total words.")
+        print("--------- Character Count -------")
+    
+        for key, value in total_chars.items():
+            print(f"{key}: {value}")
 
-    for key, value in total_chars.items():
-        print(f"{key}: {value}")
+        print("============= END ===============")
 
-    print("============= END ===============")
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(1)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        sys.exit(1)
 
 
 
